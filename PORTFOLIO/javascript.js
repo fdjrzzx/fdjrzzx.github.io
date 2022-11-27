e45383ea95e4a4fafc65ec86b418a0152e577bf9
fdjrzzx <118465998+fdjrzzx@users.noreply.github.com>
2022-11-27 13:30:15 +0700

    Add files via upload

---
+++ PORTFOLIO/javascript.js
@@ -0,0 +1,14 @@
+
+
+var el = document.getElementById("kagebunshin"); 
+function putarSuara() { 
+  el.play(); 
+} 
+
+var el = document.getElementById("rasengan"); 
+function putarMusik() { 
+  el.play(); 
+} 
+function pauseAudio() { 
+  el.pause(); 
+} 
